4dbdec4a0aa4da4abd8fb001581fb09351d62be5
i-b8o <blackview1617@gmail.com>
2022-12-01 15:54:08 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:read_only_dashboard/service/master/service.dart';
+
+
+
 
 void main() {
   runApp(const MyApp());
@@ -15,53 +17,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Main'),
+      home: const HomePage(),
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-//   final String title;
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   String _counter = "0";
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-
-//   Future<void> _getRegulation() async {
-//     var response = await MasterService.GetAllRegulations();
-//     setState(() {
-//       _counter = response.regulations;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _getRegulation,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: Column(children: const [Text('')],)),);
+  }
+}
