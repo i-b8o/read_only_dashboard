8400430bf617584e7a78bfc3e687643b7c6216cd
i-b8o <blackview1617@gmail.com>
2022-12-05 10:28:10 +0300

    added

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -6,6 +6,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: AllRegulationsWidget.create());
+    return Scaffold(
+      body: AllRegulationsWidget.create()
+      );
   }
 }
